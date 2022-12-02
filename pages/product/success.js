b76e7d480f54f72d1b337680adf46446ddd24e83
vinaymngl <asavinayaus@gmail.com>
2022-12-02 12:09:03 +0530

    changes done

--- pages/product/success.js
+++ pages/product/success.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import Link from 'next/link';
 import {BsBagCheckFill} from 'react-icons/bs';
-import {useRouter} from 'next/router';
 import { useStateContext } from "../../context/StateContext";
 import { runFireWorks } from "../../lib/utils";
 const Success = () => {
